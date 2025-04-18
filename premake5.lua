acf18d35813e24be238bbe977303cb10df47ca87
Sekt4nt <dtoronchenko@gmail.com>
2025-04-18 13:09:02 +0100

    Added premake

---
+++ premake5.lua
@@ -0,0 +1,41 @@
+workspace "MemReader"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "MemReader"
+    location "MemReader"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "On"	
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "On"
